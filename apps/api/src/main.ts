bc2d37c7c11fa9ca99ecc192a2c75b6720dae96f
andr36s <andres40106@gmail.com>
2025-07-08 15:09:57 -0500

    Agregamos la conexión para mostrar datos del backen consola del front y configuramos el packjson para iniciar con npm

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api'); // Set global prefix for all routes
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
